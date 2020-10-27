d398590cd508afbdc13799aab447849433429c2d
ramram1048 <ramram1048@gmail.com>
2020-10-27 11:39:02 +0900

    feat: express app이 /public 폴더를 static serve

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,11 +1,10 @@
+import path from 'path';
 import express from 'express';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static(path.resolve(__dirname, './public')));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
