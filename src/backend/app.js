6dda555cb73c646b604e9118d86667a03ecfcee6
ramram1048 <ramram1048@gmail.com>
2020-10-27 16:18:38 +0900

    feat(backend): backend app에서 sequelize 연동

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,12 +1,15 @@
 import path from 'path';
 import express from 'express';
 import useDevServer from './devserver';
+import db from './models';
 
 const app = express();
 const port = 3000;
 
 if (process.env.NODE_ENV === 'development') useDevServer(app);
 else app.use(express.static(path.resolve(__dirname, './public')));
+
+db.sequelize.sync();
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
