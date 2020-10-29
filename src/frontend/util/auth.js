f861922738d9e550ff5f2ee546f38c78e84a0a12
pkiop <pkiop@naver.com>
2020-10-29 16:38:44 +0900

    feat: isAuthenticated 내부 구현
    - 기존 true, false로 하드코딩되어있던 것을 로그인 인증 정보에 따라 인증하도록 수정
    - 이 함수가 실행되는 경우는 2가지가 있음
    - 1. GitHub Login 버튼을 눌러 AccessToken을 쿠키로 받아오는 경우
    - 2. LocalStorage에 있는 AccessToken을 가지고 인증하는 경우
    
    - 1번 경우에 받아온 쿠키를 LocalStorage에 저장하고 삭제하도록 함

--- src/frontend/util/auth.js
+++ src/frontend/util/auth.js
@@ -1,8 +1,30 @@
-export default {
-  isAuthenticated: () => true,
-};
+import cookie from './cookie';
 
-// function getCookie(name) {
-//   const value = document.cookie.match(`(^|;) ?${name}=([^;]*)(;|$)`);
-//   return value ? value[2] : null;
-// }
+export default {
+  isAuthenticated: () => {
+    const cookieAccessToken = cookie.getCookie('accessToken') || null;
+    const localStorageAccessToken = window.localStorage.getItem('accessToken') || null;
+
+    let accessToken = null;
+    if (cookieAccessToken !== null) {
+      cookie.deleteCookie('accessToken');
+      accessToken = cookieAccessToken;
+      window.localStorage.setItem('accessToken', accessToken);
+    } else if (localStorageAccessToken !== null) {
+      accessToken = localStorageAccessToken;
+    } else {
+      return false;
+    }
+
+    return fetch('/api/auth/profile', {
+      headers: {
+        Authorization: `bearer ${accessToken}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 403) return false;
+        return true;
+      })
+      .catch(console.error);
+  },
+};
