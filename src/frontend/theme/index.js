35a4325b806be14577442bc8e7612b2b9e77563e
ramram1048 <ramram1048@gmail.com>
2020-10-29 16:34:03 +0900

    feat: globalStyle 적용
    - index.html에 적용한 style을 css in js형태로 적용

--- src/frontend/theme/index.js
+++ src/frontend/theme/index.js
@@ -1,4 +1,24 @@
-export default {
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+    min-height: 100vh;
+  }
+  button {
+    border: none;
+    &:not(:disabled) {
+      cursor: pointer;
+    }
+  }
+`;
+
+export const theme = {
   mainColor: '',
   textColor: '#212121',
 };
