d80e2e98df1bbf6f20599b6bef8a400452c23fb7
ramram1048 <ramram1048@gmail.com>
2020-11-03 13:18:52 +0900

    feat: backend /issues endpoint 지정

---
+++ src/backend/controllers/issues.js
@@ -0,0 +1,19 @@
+export const getAllIssues = (req, res, next) => {
+  res.send({});
+};
+
+export const getIssueDetail = (req, res, next) => {
+  res.send({});
+};
+
+export const patchIssue = (req, res, next) => {
+  res.send({});
+};
+
+export const modifyIssueStatus = (req, res, next) => {
+  res.send({});
+};
+
+export const postIssue = (req, res, next) => {
+  res.send({});
+};
--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import AuthRouter from './auth';
+import IssueRouter from './issues';
 
 const router = Router();
 
 router.use('/auth', AuthRouter);
+router.use('/issues', IssueRouter);
 
 export default router;
