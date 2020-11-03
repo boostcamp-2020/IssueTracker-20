0279b7646f6579e174454e67922b03552bafb083
Park Ji Hong <hongzzi.dev@gmail.com>
2020-11-03 16:49:45 +0900

    feat: labels API close #10
    - 레이블 목록 가져오는 api 작성

--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import AuthRouter from './auth';
+import IssueRouter from './issues';
+import LabelRouter from './labels';
 
 const router = Router();
 
 router.use('/auth', AuthRouter);
+router.use('/issues', IssueRouter);
+router.use('/labels', LabelRouter);
 
 export default router;
