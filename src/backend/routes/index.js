fd4842677c6ca503216484ff1d17969b0601ec2f
Park Ji Hong <hongzzi.dev@gmail.com>
2020-11-03 17:08:44 +0900

    feat: milestone API 작성 #11
    - milestone router 등록, milestone 목록 api 작성

--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import AuthRouter from './auth';
 import IssueRouter from './issues';
 import LabelRouter from './labels';
+import MilestoneRouter from './milestones';
 
 const router = Router();
 
 router.use('/auth', AuthRouter);
 router.use('/issues', IssueRouter);
 router.use('/labels', LabelRouter);
+router.use('/milestones', MilestoneRouter);
 
 export default router;
