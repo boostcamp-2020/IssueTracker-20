bac780de66647f07e7664db105ea18edb74590b7
Park Ji Hong <hongzzi.dev@gmail.com>
2020-11-03 23:09:17 +0900

    fix: 충돌난 파일의 메소드 복구
    #71 메소드 재작성

--- src/backend/controllers/issues.js
+++ src/backend/controllers/issues.js
@@ -69,14 +69,56 @@ export const getIssueDetail = (req, res) => {
   res.json({});
 };
 
-export const patchIssue = (req, res) => {
-  res.json({});
+export const patchIssue = async (req, res, next) => {
+  try {
+    const Issue = { ...req.body };
+    await db.Issue.update(Issue, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ message: 'modify success' });
+  } catch (error) {
+    res.status(500).json({ message: 'db error' });
+  }
 };
 
-export const modifyIssueStatus = (req, res) => {
-  res.json({});
+export const modifyIssueStatus = async (req, res, next) => {
+  try {
+    await db.Issue.update(
+      { isOpened: req.body.isOpen },
+      {
+        where: {
+          id: {
+            [db.Sequelize.Op.in]: req.body.issues,
+          },
+        },
+      },
+    );
+    res.status(200).json({ message: 'modify status success' });
+  } catch (error) {
+    res.status(500).json({ message: 'db error' });
+  }
 };
 
-export const postIssue = (req, res) => {
-  res.json({});
+export const postIssue = async (req, res, next) => {
+  try {
+    const Issue = {
+      title: req.body.title,
+      UserId: req.user.id,
+      createDate: new Date(),
+      isOpened: true,
+    };
+    const issue = await db.Issue.create(Issue);
+    const Comment = {
+      content: req.body.content,
+      createDate: new Date(),
+      issueId: issue.id,
+      UserId: 1,
+    };
+    await db.Comment.create(Comment);
+    res.status(200).json({ id: issue.id, message: 'create success' });
+  } catch (error) {
+    res.status(500).json({ message: 'db error' });
+  }
 };
