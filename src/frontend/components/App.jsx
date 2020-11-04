1f69343c6fc22fa8f3f981e4cc45eed3b40aa6d3
Park Ji Hong <hongzzi.dev@gmail.com>
2020-11-04 15:18:49 +0900

    feat: 이슈 템플릿 route추가
    - path추가

--- src/frontend/components/App.jsx
+++ src/frontend/components/App.jsx
@@ -1,26 +1,24 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import {
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import LoginPage from './Login';
 import IssuePage from './Issue';
-import Main from './Main';
+import IssueForm from './IssueForm';
 import PrivateRoute from './PrivateRoute';
 
 const App = () => {
   console.log('App loaded!');
 
   return (
-      <div>
-        <Switch>
-          <PrivateRoute exact path="/">
-            <IssuePage />
-          </PrivateRoute>
-          <Route path="/login" component={LoginPage} />
-        </Switch>
-      </div>
+    <div>
+      <Switch>
+        <PrivateRoute exact path="/">
+          <IssuePage />
+        </PrivateRoute>
+        <Route exact path="/issue/template" component={IssueForm} />
+        <Route path="/login" component={LoginPage} />
+      </Switch>
+    </div>
   );
 };
 
