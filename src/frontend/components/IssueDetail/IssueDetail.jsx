494fc267a8c6c8e03453ad01f364900524f18a29
Park Ji Hong <hongzzi.dev@gmail.com>
2020-11-05 15:24:40 +0900

    feat: get issue api 결과값 이용해서 디테일 컴포넌트에 정보 출력

--- src/frontend/components/IssueDetail/IssueDetail.jsx
+++ src/frontend/components/IssueDetail/IssueDetail.jsx
@@ -1,39 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import styled from 'styled-components';
 import { useParams } from 'react-router';
 
 import useFetch from '@Util/useFetch';
+import { calculateTimeDiff } from '@Util/date';
 import Sidebar from '@Components/Sidebar';
 import CommentList from './CommentList.jsx';
 
+const getIssueTimeBoard = ({ isOpened, createDate }) => {
+  return isOpened
+    ? `opened this issue ${calculateTimeDiff(createDate)}`
+    : `closed ${calculateTimeDiff(createDate)}`;
+};
+
 const IssueDetail = () => {
   const { id } = useParams();
-  const [issue, setIssue] = useState({});
+  const [issue, setIssue] = useState();
 
   useEffect(async () => {
     const result = await useFetch(`/api/issues/${id}`, 'GET');
-    console.log(result);
+    const timeBoard = getIssueTimeBoard(result.issue);
+    setIssue({ ...result.issue, timeBoard });
+    console.log(result.issue);
   }, []);
 
   return (
-    <>
-      <Topbar>ISSUE CRACKER</Topbar>
-      <Wrapper>
-        <Header>
-          <HeaderTitle>
-            <h1>헤더다. #1</h1>
-            <button>edit</button>
-          </HeaderTitle>
-          <HeaderStatus>open, close 상태값, author</HeaderStatus>
-        </Header>
-        <Container>
-          <CommentContainer>
-            <CommentList />
-          </CommentContainer>
-          <Sidebar />
-        </Container>
-      </Wrapper>
-    </>
+    <Fragment>
+      {issue && (
+        <Fragment>
+          <Topbar>ISSUE CRACKER</Topbar>
+          <Wrapper>
+            <Header>
+              <HeaderTitle>
+                <H1>
+                  <IssueTitle>{issue.title}</IssueTitle>
+                  <IssueNumber>#{issue.id}</IssueNumber>
+                </H1>
+                <EditButton>edit</EditButton>
+              </HeaderTitle>
+              <HeaderStatus>
+                <StatusLabel isOpened={issue.isOpened}>
+                  {issue.isOpened ? 'open' : 'close'}
+                </StatusLabel>
+                <UserName>{issue.author?.username}</UserName>
+                <TimeBoard>{issue.timeBoard}</TimeBoard>·
+                <CommentCount>{issue.comments?.length} comment</CommentCount>
+              </HeaderStatus>
+            </Header>
+            <Container>
+              <CommentContainer>
+                <CommentList content={issue.content} list={issue.comments} />
+              </CommentContainer>
+              <Sidebar />
+            </Container>
+          </Wrapper>
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
 
@@ -78,10 +101,64 @@ const HeaderTitle = styled.div`
   align-items: center;
 `;
 
-const HeaderStatus = styled.div``;
+const H1 = styled.div`
+  display: flex;
+  flex-direction: row;
+  
+  font-size: 36px;
+  padding: 1.5rem 0 1rem 0;
+`;
+
+const IssueTitle = styled.div`
+  font-weight: 500;
+`;
+
+const IssueNumber = styled.div`
+  color: lightgray;
+  font-weight: 300;
+
+  padding: 0 1rem;
+`;
+
+const EditButton = styled.button`
+  padding: 0.5rem 1rem;
+  border-radius: 5px;
+`;
+
+const HeaderStatus = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  color: #888;
+`;
+
+const StatusLabel = styled.div`
+  color: white;
+  font-weight: 500;
+
+  background: ${(props) => (props.isOpened ? '#5EBB5C' : '#BA3638')};
+  border-radius: 5px;
+  padding: 0.35rem 1rem;
+`;
+
+const UserName = styled.div`
+  color: #777777;
+  font-weight: bold;
+
+  padding: 0 0.2rem 0 0.6rem;
+`;
 
 const CommentContainer = styled.div`
   flex: 1;
+`;
+
+const TimeBoard = styled.div`
+  padding: 0 0.3rem;
+`;
+
+const CommentCount = styled.div`
+  padding: 0 0.3rem;
 `;
 
 export default IssueDetail;
