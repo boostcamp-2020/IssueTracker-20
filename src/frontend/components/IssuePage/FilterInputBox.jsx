3daa68bf01e40294ca80ff407a083295f1fb6e17
pkiop <pkiop@naver.com>
2020-11-09 17:45:10 +0900

    fix: filter입력 값 filter상태로 변환하는 코드 작성

--- src/frontend/components/IssuePage/FilterInputBox.jsx
+++ src/frontend/components/IssuePage/FilterInputBox.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
@@ -20,15 +21,29 @@ const Main = styled.input`
 
 const getFilterAllValue = (filter) => {
   let res = '';
-  // eslint-disable-next-line no-restricted-syntax
+  // eslint-disable-next-line guard-for-in
   for (const el in filter) {
-    if (el === 'is') {
-      res += `${el}:${filter[el]} `;
-    } else {
-      res += filter[el].reduce((acc, e) => `${el}:${acc} ${el}:${e} `);
-    }
+    res += filter[el].reduce((acc, e) => `${acc}${el}:${e} `, '');
   }
 
+  return res;
+};
+
+const refreshValue = (value) => {
+  const res = {
+    is: [],
+    author: [],
+    assignee: [],
+    label: [],
+    milestone: [],
+  };
+  const test1 = value.split(' ');
+  test1.forEach((el) => {
+    const test2 = el.split(':');
+    if (test2[0]) {
+      res[test2[0]].push(test2[1]);
+    }
+  });
   return res;
 };
 
@@ -40,12 +55,13 @@ const FilterInputBox = (props) => {
 
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
-      setFilter({ type: 'ADD' }, 'test');
+      const refresh = refreshValue(event.target.value);
+      setFilter({ type: 'SET', values: refresh });
     }
   };
 
   return (
-  <Main defaultValue={getFilterAllValue(filter)} onKeyDown={handleKeyDown}/>
+    <Main defaultValue={getFilterAllValue(filter)} onKeyDown={handleKeyDown}/>
   );
 };
 
