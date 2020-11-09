22d7bcc1d86490250ac476ae081ea9760a6df325
leejiwoo2002 <leejiwoo2004@kyonggi.ac.kr>
2020-11-09 15:22:41 +0900

    feat : Label Create 라우터 구현
    - req.body
      - title
      - description
      - color
    - Post 요청시 위 body를 받아와 sequlize.create

--- src/backend/controllers/labels.js
+++ src/backend/controllers/labels.js
@@ -9,8 +9,19 @@ export const getAllLabels = async (req, res, next) => {
   }
 };
 
-export const createLabel = async (req, res, next) => {
-  res.json({});
+export const createLabel = async (req, res) => {
+  try {
+    const Label = {
+      title: req.body.title,
+      description: req.body.description,
+      color: req.body.color,
+    };
+    const label = await db.Label.create(Label);
+
+    res.status(200).json({ id: label.get('id'), message: 'create success' });
+  } catch (error) {
+    res.status(500).json({ id: null, message: `${error}` });
+  }
 };
 
 export const updateLabel = async (req, res, next) => {
