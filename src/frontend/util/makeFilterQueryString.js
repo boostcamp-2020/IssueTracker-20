fbda61666012f0ea10eb5322c673b2193f4e56a1
pkiop <pkiop@naver.com>
2020-11-09 17:39:25 +0900

    fix: makeFilterQueryString filter가 object일 때 동작하도록 수정

--- src/frontend/util/makeFilterQueryString.js
+++ src/frontend/util/makeFilterQueryString.js
@@ -2,7 +2,7 @@ const makeFilterQueryString = (filter) => {
   let res = '';
   // eslint-disable-next-line no-restricted-syntax
   for (const [el, value] of Object.entries(filter)) {
-    res += `${el}=${value}&`;
+    res += value.reduce((acc, e) => `${acc}${el}=${e}&`, '');
   }
   return res.substr(0, res.length - 1);
 };
