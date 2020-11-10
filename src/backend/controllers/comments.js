4e87211b97f3eb74a65521ee6c45dfdc5505a5e5
Park Ji Hong <hongzzi.dev@gmail.com>
2020-11-10 18:14:39 +0900

    fix: comment api 에러 수정
    - import { method... } from ____ 으로 불러오기위해서 export 형식 변경

--- src/backend/controllers/comments.js
+++ src/backend/controllers/comments.js
@@ -1,6 +1,6 @@
 import db from '../models';
 
-const postComment = async (req, res) => {
+export const postComment = async (req, res) => {
   try {
     const Comment = {
       content: req.body.content,
@@ -8,11 +8,10 @@ const postComment = async (req, res) => {
       issueId: req.body.issueId,
       authorId: req.user.get('id'),
     };
+    console.log(Comment);
     await db.Comment.create(Comment);
     res.status(200).json({ message: 'create success' });
   } catch (error) {
     res.status(500).json({ id: null, message: `${error}` });
   }
 };
-
-export default postComment;
