d3883bf993f14ca6f008a36c6c6a6deadbafd1cd
leejiwoo2002 <leejiwoo2004@kyonggi.ac.kr>
2020-11-10 13:48:58 +0900

    feat : milestone 페이지 목록 틀 생성
    - 목록 상태바 생성
      - opened, closed 버튼 배치
    - 나열될 목록들 추가 예정

--- src/frontend/components/MilestonePage/MilestonePage.jsx
+++ src/frontend/components/MilestonePage/MilestonePage.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import LabelIcon from '@Images/comment.svg';
+import MilestoneIcon from '@Images/milestone.svg';
+import Button from '@Common/Button';
 
 const MilestonePage = () => {
   const value = 0;
   return (
       <Main>
           <Content>
-
+          <FlexRowBar>
+            <MenuBox>
+              <LabelLinkButton><LabelIcon></LabelIcon>Label</LabelLinkButton>
+              <MilestoneLinkButton><MilestoneIcon></MilestoneIcon>Milestone</MilestoneLinkButton>
+             </MenuBox>
+             <Button
+              type="confirm"
+              text="New Milestone"
+              />
+            </FlexRowBar>
+            <FlexBoxContainer>
+          <FlexColumnBar>
+            <MenuBar>
+                <MenuBox>
+                  <OpenStatusButton><MilestoneIcon></MilestoneIcon> 0 Opened</OpenStatusButton>
+                  <OpenStatusButton> 0 Closed</OpenStatusButton>
+                </MenuBox>
+            </MenuBar>
+            hello
+          </FlexColumnBar>
+        </FlexBoxContainer>
           </Content>
       </Main>
 
@@ -34,6 +57,69 @@ const Content = styled.div`
   ${FlexColumnBox}
   width: 1200px;
   height: 100%;
+`;
+
+const MenuBox = styled.div`
+  ${FlexRowBox}
+  position: relative;
+`;
+
+const LabelLinkButton = styled.button`
+  border: 1px solid;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top-left-radius: 6px;
+  border-bottom-left-radius: 6px;
+  width: 6rem;
+  height: 2rem;
+`;
+
+const MilestoneLinkButton = styled.button`
+  border: 1px solid;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top-right-radius: 6px;
+  border-bottom-right-radius: 6px;
+  width: 8rem;
+  height: 2rem;
+`;
+
+const FlexRowBar = styled.div`
+  ${FlexRowBox}
+  width: 100%;
+  height: 2rem;
+  justify-content: space-between;
+`;
+
+const FlexBoxContainer = styled.div`
+  padding: 1.2rem 0;
+`;
+
+const FlexColumnBar = styled.div`
+  ${FlexColumnBox}
+  border: 1px solid ${(props) => props.theme.menuBarBorderColor};
+  border-radius: 6px;
+`;
+
+const MenuBar = styled.div`
+  ${FlexRowBox}
+  justify-content: space-between;
+  box-sizing: border-box;
+  background-color: ${(props) => props.theme.menuBarBgColor};
+  width: 100%;
+  padding: 0.8rem;
+  font-size: 1rem;
+`;
+
+const OpenStatusButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color : transparent;
+  width: 6rem;
+  height: 2rem;
 `;
 
 export default MilestonePage;
