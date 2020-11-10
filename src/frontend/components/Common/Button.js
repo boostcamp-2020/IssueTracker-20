e4246d2a3d37892a2d0762c5108fba11fffb19c3
ramram1048 <ramram1048@gmail.com>
2020-11-10 16:44:04 +0900

    feat(CommonButton): JSX type attribute를 사용할 수 있게 대응
    - 기존 코드 수정 없이 하도록 내부에서 `type` prop을 다른 이름으로 받게함
    - JSX type attribute는 htmlType이라는 prop으로 전달하면 동작하게됨

--- src/frontend/components/Common/Button.js
+++ src/frontend/components/Common/Button.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Button = ({
-  type, text, onClick, valid,
+  type: themeType, text, onClick, valid, htmlType,
 }) => (
-  <CustomButton onClick={onClick} type={type} valid={valid}>
+  <CustomButton onClick={onClick} type={htmlType} themeType={themeType} valid={valid}>
     {text}
   </CustomButton>
 );
@@ -15,6 +15,7 @@ Button.propTypes = {
   text: PropTypes.string,
   onClick: PropTypes.func,
   valid: PropTypes.bool,
+  htmlType: PropTypes.oneOf(['submit', 'reset', 'button']),
 };
 
 Button.defaultProps = {
@@ -25,13 +26,13 @@ const CustomButton = styled.button`
   all: unset;
   cursor: pointer;
   background: ${(props) => {
-    if (props.type === 'cancel') {
+    if (props.themeType === 'cancel') {
       return props.theme.subButtonColor;
     }
     return props.valid ? props.theme.buttonColor : props.theme.unActiveButtonColor;
   }};
   color: ${(props) => {
-    if (props.type === 'cancel') {
+    if (props.themeType === 'cancel') {
       return props.theme.textColor;
     }
     return props.theme.whiteColor;
