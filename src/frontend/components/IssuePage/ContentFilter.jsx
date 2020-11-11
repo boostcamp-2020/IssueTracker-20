98f4efd9ceb7205ff0639d91a5266326947e3ec8
pkiop <pkiop@naver.com>
2020-11-11 15:49:11 +0900

    fix: filterDispatch 넘겨주도록 수정

--- src/frontend/components/IssuePage/ContentFilter.jsx
+++ src/frontend/components/IssuePage/ContentFilter.jsx
@@ -23,6 +23,7 @@ const ContentFilter = (props) => {
       <SortMenuButton onClick={() => setBoxVisible()}>{name}</SortMenuButton>
       {boxVisible
       && <ContentDropDown
+          filterDispatch={dropDownValues.filterDispatch}
           fetchLink={dropDownValues.fetchLink}
           name={name}
           istitleBold={dropDownValues.istitleBold}
