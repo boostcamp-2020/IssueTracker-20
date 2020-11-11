1a410bdafe66d97adc5d7f22bd729adad8e7d87c
pkiop <pkiop@naver.com>
2020-11-11 15:16:55 +0900

    config: filter버튼 configuration 모아두는 파일 생성

---
+++ src/frontend/components/IssuePage/filterButtonConfig.js
@@ -0,0 +1,49 @@
+const authorConfig = (filterDispatch) => ({
+  name: 'Author',
+  dropDownValues:
+    {
+      filterDispatch,
+      fetchLink: 'assignees',
+      filter: 'author',
+      name: 'Author',
+      isTitleBold: true,
+    },
+});
+
+const assigneeConfig = (filterDispatch) => ({
+  name: 'Assignee',
+  dropDownValues:
+    {
+      filterDispatch,
+      fetchLink: 'assignees',
+      filter: 'assignees',
+      name: 'Assignee',
+      isTitleBold: true,
+    },
+});
+
+const labelsConfig = (filterDispatch) => ({
+  name: 'Label',
+  dropDownValues:
+    {
+      filterDispatch,
+      fetchLink: 'labels',
+      filter: 'labels',
+      name: 'Label',
+      isTitleBold: true,
+    },
+});
+
+const milestoneConfig = (filterDispatch) => ({
+  name: 'Milestone',
+  dropDownValues:
+    {
+      filterDispatch,
+      fetchLink: 'milestones',
+      filter: 'milestone',
+      name: 'Milestone',
+      isTitleBold: true,
+    },
+});
+
+export default (filterDispatch) => [authorConfig(filterDispatch), assigneeConfig(filterDispatch), labelsConfig(filterDispatch), milestoneConfig(filterDispatch)];
