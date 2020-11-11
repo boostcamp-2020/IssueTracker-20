7ce88af595cc94f72d1187e0a59a0412db0602bd
pkiop <pkiop@naver.com>
2020-11-11 17:27:49 +0900

    feat: sidebar 드랍다운 버튼 추가

--- src/frontend/components/Sidebar/Sidebar.jsx
+++ src/frontend/components/Sidebar/Sidebar.jsx
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import styled from 'styled-components';
+import DropDownButton from '@Components/Sidebar/DropDownButton';
+import { assigneeConfig, labelsConfig, milestoneConfig } from '@Components/Sidebar/ButtonConfig';
 
-import GearIcon from '@Images/GearIcon.svg';
+export const filterReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET': {
+      console.log('SET');
+      return ['2'];
+    }
+    case 'REPLACE': {
+      console.log('replace');
+      return ['1'];
+    }
+    default: {
+      console.error('잘못된 타입입니다.');
+      return { error: '잘못된 타입 에러' };
+    }
+  }
+};
 
-const Sidebar = () => (
+const Sidebar = () => {
+  console.log('assigneeConfig : ', assigneeConfig);
+  const [assignee, assigneeDispatch] = useReducer(filterReducer, []);
+  const [labels, labelsDispatch] = useReducer(filterReducer, []);
+  const [milestone, milestoneDispatch] = useReducer(filterReducer, []);
+
+  return (
   <Wrapper>
     <AssigneeBox>
-      <BoxHeader>
-        <Title>Assignees</Title>
-        <GearIcon />
-      </BoxHeader>
+      <DropDownButton name={'Assignees'} dropDownValues={assigneeConfig(assigneeDispatch).dropDownValues}/>
       <BoxBody>No one-assign yourself</BoxBody>
     </AssigneeBox>
     <LabelBox>
-      <BoxHeader>
-        <Title>Labels</Title>
-        <GearIcon />
-      </BoxHeader>
+      <DropDownButton name={'Labels'} dropDownValues={labelsConfig(labelsDispatch).dropDownValues}/>
       <BoxBody>None yet</BoxBody>
     </LabelBox>
     <MilestoneBox>
-      <BoxHeader>
-        <Title>Milestones</Title>
-        <GearIcon />
-      </BoxHeader>
+      <DropDownButton name={'Milestones'} dropDownValues={milestoneConfig(milestoneDispatch).dropDownValues}/>
       <BoxBody>No milestone</BoxBody>
     </MilestoneBox>
   </Wrapper>
-);
+  );
+};
 
 const Wrapper = styled.div`
   display: flex;
@@ -51,24 +66,6 @@ const MilestoneBox = styled.div`
   padding: 0.7rem 0;
   border-bottom: 1px solid #ddd;
 `;
-
-const BoxHeader = styled.div`
-  cursor: pointer;
-
-  display: flex;
-  justify-content: space-between;
-
-  font-weight: bold;
-  font-size: 14px;
-  color: gray;
-  padding: 0.5rem 0;
-
-  &:hover {
-    color: #11aaff;
-  }
-`;
-
-const Title = styled.div``;
 
 const BoxBody = styled.div`
   font-size: 13px;
