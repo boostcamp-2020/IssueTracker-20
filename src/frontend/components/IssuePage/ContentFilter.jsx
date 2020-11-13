017682c743e35a49ef7116be0e3393b9d9841e50
pkiop <pkiop@naver.com>
2020-11-13 11:21:03 +0900

    fix: 필터버튼 스타일 수정

--- src/frontend/components/IssuePage/ContentFilter.jsx
+++ src/frontend/components/IssuePage/ContentFilter.jsx
@@ -10,14 +10,32 @@ const SortMenuArea = styled.div`
 
 const SortMenuButton = styled.button`
   height: 1rem;
+  font-size: 0.95rem;
+  color: rgba(0,0,0,0.7);
   border: none;
   cursor: pointer;
   background-color: transparent;
+  text-align: center;
+  padding: 0 0.8rem;
 `;
 
 const Wrap = styled.div`
   all:unset;
   ${(props) => (props.visible ? '' : 'display:none;')}
+`;
+
+const DownTriangle = styled.span`
+  display: inline-block;
+  width: 0;
+  height: 0;
+  vertical-align: middle;
+  content: "";
+  border-top-style: solid;
+  border-top-width: 4px;
+  border-right: 4px solid transparent;
+  border-bottom: 0 solid transparent;
+  border-left: 4px solid transparent;
+  padding-left: 1px;
 `;
 
 const ContentFilter = (props) => {
@@ -26,7 +44,11 @@ const ContentFilter = (props) => {
 
   return (
     <SortMenuArea>
-      <SortMenuButton onClick={() => setBoxVisible()}>{name}</SortMenuButton>
+      <SortMenuButton onClick={() => setBoxVisible()}>
+        {name}
+        {' '}
+        <DownTriangle />
+      </SortMenuButton>
         <Wrap visible={boxVisible}>
           <DropDownCover onClick={() => setBoxVisible()}/>
           <ContentDropDown
